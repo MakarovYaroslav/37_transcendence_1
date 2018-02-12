f585411a8ede20fb6aa01bd531783c36fd85c0f7
MakarovYaroslav <yaroslav.makarov.97@mail.ru>
2018-02-12 21:41:29 +0300

    Изменено отображение инфо о пользователе

--- transcendence/views.py
+++ transcendence/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import TemplateView
 from datetime import datetime
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 
 
 class IndexView(TemplateView):
@@ -8,4 +10,13 @@ class IndexView(TemplateView):
 
 class UserInfoView(TemplateView):
     template_name = "transcendence/user_info.html"
-    registration_date = datetime.now()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user_id = context['user_id']
+        try:
+            user = User.objects.get(id=user_id)
+        except User.DoesNotExist:
+            user = None
+        context['user'] = user
+        return context
