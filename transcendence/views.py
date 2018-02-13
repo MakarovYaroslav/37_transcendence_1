3084e492e7b13043b1b93ac0f6255fa37bbf1f96
MakarovYaroslav <yaroslav.makarov.97@mail.ru>
2018-02-13 19:41:10 +0300

    Добавлен DetailView для отображения инфо о пользователе

--- transcendence/views.py
+++ transcendence/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
 from django.contrib.auth.models import User
 
 
@@ -6,15 +6,7 @@ class IndexView(TemplateView):
     template_name = "transcendence/index.html"
 
 
-class UserInfoView(TemplateView):
+class UserInfoView(DetailView):
     template_name = "transcendence/user_info.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        user_id = context['user_id']
-        try:
-            user = User.objects.get(id=user_id)
-        except User.DoesNotExist:
-            user = None
-        context['user'] = user
-        return context
+    model = User
+    pk_url_kwarg = 'user_id'
