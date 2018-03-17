0132971d6d94f377aaf6b4678763d28bc8bd9255
MakarovYaroslav <yaroslav.makarov.97@mail.ru>
2018-03-17 08:51:56 +0300

    Добавлено описание БД

--- social_network/settings.py
+++ social_network/settings.py
@@ -61,10 +61,10 @@ class BaseConfig(Configuration):
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': os.getenv('DB_NAME'),
-            'USER': os.getenv('DB_USER'),
-            'PASSWORD': os.getenv('DB_PASSWORD'),
-            'HOST': os.getenv('DB_HOST'),
-            'PORT': os.getenv('DB_PORT'),
+            'USER': 'postgres',
+            'PASSWORD': '',
+            'HOST': 'localhost',
+            'PORT': '5432',
         }
     }
 
